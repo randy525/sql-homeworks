9d9fd92632d3edb06112139f59733a3e3b585e2b
Ilia Simlih <ilia.simlih@endava.com>
2021-10-05 18:25:05 +0300

    completed sql migration homework

--- src/main/resources/db/migration/V20191004033400__create_table_hours_worked.sql
+++ src/main/resources/db/migration/V20191004033400__create_table_hours_worked.sql
@@ -2,5 +2,6 @@ CREATE TABLE hours_worked
 (
     project_id NUMBER(6),
     employee_id NUMBER(6),
-    hours_amount NUMBER(6, 2) NOT NULL
+    hours_amount NUMBER(6, 2) NOT NULL,
+    PRIMARY KEY (project_id, employee_id)
 );
